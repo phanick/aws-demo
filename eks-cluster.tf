3356038c231a1bcb188b85b9584a769ac4ff123f
phanick <39258517+phanick@users.noreply.github.com>
2022-07-01 19:42:43 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.20"
+  cluster_version = "1.22"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
@@ -33,6 +33,3 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
