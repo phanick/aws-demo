23731f8a56bf73045ae999bed25421be2a1a9904
phanick <39258517+phanick@users.noreply.github.com>
2022-07-01 18:50:47 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -17,14 +17,14 @@ module "eks" {
       instance_type                 = "t2.small"
       additional_userdata           = "echo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 0
     },
     {
       name                          = "worker-group-2"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 0
     },
   ]
 }
